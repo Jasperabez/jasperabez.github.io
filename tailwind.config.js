b15685675605d368faf62a13ffbcb025e304a534
Jabez Tho <jabeztho@gmail.com>
2023-03-08 15:28:06 +0800

    new badge partial layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,13 @@ module.exports = {
         iot: '#00BFFF',
         mqtt: '#FFA500',
         aws: '#FF9900',
-        vr: '#FF4081'
+        vr: '#FF4081',
+        ar: '#00FF00',
+        python: '#4B8BBE',
+        windowsapi: '#0078D7',
+        bluetooth: '#0A84FF',
+        networking: '#007ACC',
+        javascript: '#F7DF1E'
       },
       typography: {
         DEFAULT: {
